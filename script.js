1d189accda2bba180b2c7063a23fd511846e0b02
idriss <idrissaamadouousmane@gmail.com>
2023-08-16 14:11:46 +0100

    Projet final

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Créer un événement au scroll
 
-// Réduire la navbar quand on descend vers le vide, la remettre à sa taille initiale si on remonte tout en haut
+// Réduire la navbar quand on descend vers le bas, la remettre à sa taille initiale si on remonte tout en haut
 
 // Faire apparaitre l'image de la partie improvise
 
 // Faire apparaitre la popup quand on est en bas du site
 
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
-let playOne = true;
+let playOne = true; // permettre une seule fois la lecture.
 
 window.addEventListener("scroll", () => {
-    console.log(window.scrollY);
+    // console.log(window.scrollY);
     if (window.scrollY > 90) {
         navbar.style.height = "40px";
     } else {
